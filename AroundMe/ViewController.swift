1061130096e586c71050c45a812f32ede37dc7d6
Nicolas Schelkens <schelkensnicolas@hotmail.com>
2015-12-19 10:33:51 +0100

    Initial project

---
+++ AroundMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AroundMe
+//
+//  Created by Nicolas Schelkens on 19/12/15.
+//  Copyright © 2015 Nicolas Schelkens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
